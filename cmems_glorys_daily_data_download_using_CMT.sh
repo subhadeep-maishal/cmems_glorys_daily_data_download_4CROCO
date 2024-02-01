7eedbdf6e5276f4f7cb9b92b24ff91294df75e55
SUBHADEEP MAISHAL <subharsgis08@gmail.com>
2024-02-01 19:03:24 +0530

    Update cmems_glorys_daily_data_download_using_CMT.sh

--- cmems_glorys_daily_data_download_using_CMT.sh
+++ cmems_glorys_daily_data_download_using_CMT.sh
@@ -20,8 +20,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA  02111-1307  USA
 #
-# Copyright (c) 2018 S. Jullien
-# swen.jullien@ifremer.fr
 # Author : S. Maishal
 # subhadeepmaishal@kgpian.iitkgp.ac.in
 
